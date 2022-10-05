8bfe458f5494cd38ef909686f51b12c93b9c1320
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-05 12:28:41 +0200

    Bugfix

--- src/App.java
+++ src/App.java
@@ -2,10 +2,10 @@ import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("For tradicional: "   + ejercicio_1());
-        System.out.println("While tradicional: " + ejercicio_1_while());
-        System.out.println("Funcional: "         + ejercicio_1_funcional());
-        ejercicio_2();
+        System.out.println("For tradicional: "       + ejercicio_1());
+        System.out.println("While tradicional: "     + ejercicio_1_while());
+        System.out.println("Funcional: "             + ejercicio_1_funcional());
+        System.out.println("For con restricciones: " + ejercicio_2());
     }
 
 
@@ -43,12 +43,13 @@ public class App {
         // Restricción: la suma no puede ser mayor que 100
         
         int sum = 0;
-        
-        for (int i = 0; i < 100 && sum < 100; i++) {
+        int i = 0;
+
+        for (i = 0; i < 100 && sum < 100; i++) {
             sum += i;
         }
 
-        System.out.println("El último valor sumado ha sido " + sum);
+        System.out.println("El último valor sumado ha sido " + (i-1));
         
         return sum;
     }
