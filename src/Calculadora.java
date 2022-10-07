c39612719a2500cd6cd37d45ebd2d9f0c7f24948
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-07 10:31:51 +0200

    Separamos controlador y calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,77 +1,35 @@
+import java.lang.reflect.Constructor;
+
 public class Calculadora {
-    public static void main(String[] args) {
-        /* Cosas que va ahacer mi calculadora 
-         * Preguntar por la operación
-         * Leer los numeros a y b
-         * Operar e imprimir la respuesta
-        */
-        boolean exit = false;
+    private CalcOperator operator = null;
 
-        do {
-            System.out.println(
-                "Hola, ¿Qué operación quieres realizar?\n"
-                + "\t +, -, *, / \n"   
-            );
-            System.out.println(
-                "Pulsa q para salir\n"   
-            );
-            
-            CalcOperator operator = null;   // FIXME
-            
-            do {
-                String readValue = System.console().readLine();
-                if (readValue.toLowerCase().compareTo("q") == 0) {
-                    exit = true;
-                }
-                else if (readValue.compareTo("+") == 0){
-                    operator = new Add();
-                }
-                else if (readValue.compareTo("-") == 0){
-                    operator = new Substract();
-                }
-                else if (readValue.compareTo("*") == 0){
-                    operator = new Multiplication();
-                }
-                else if (readValue.compareTo("/") == 0){
-                    operator = new Division();
-                }
-                else{
-                    System.out.println("Esa operación no está definida");
-                }
-            } while((operator == null)&&(!exit));
-
-            if(!exit){
-
-                System.out.println("\nEscriba el valor de a\n");
-                double a = readDouble();
-    
-                System.out.println("\nEscriba el valor de b\n");
-                double b = readDouble();
-                
-                double result = operator.operate(a, b);
-    
-                System.out.println("\nEl resultado es\n " + result);        
-            }
-        
-        } while (!exit);
+    public Calculadora () {
+        this.operator = null;
     }
 
+    public Calculadora (CalcOperator operator) {
+        this.operator = operator;
+    }
     
-    private static double readDouble() {
-        boolean is_double = false;
-        double     number    = 0;
+    public CalcOperator getOperator() {
+        return operator;
+    }
+    
+    public void setOperator(CalcOperator operator) {
+        this.operator = operator;
+    }
 
-        do {
-            try {
-                number = Double.parseDouble(System.console().readLine());
-                is_double = true;
-            }
-            catch(NumberFormatException e){
-                System.out.println("Debes introducir un número");
-                is_double = false;
-            }
-        }while(!is_double);
+    public double calculate (double a, double b) {
+        double result = 0;
+        
+        try {
+            result = operator.operate(a, b);
+        }
+        catch (ArithmeticException e) {
 
-        return number;
+        }
+
+        return result;
     }
 }
+        
---
+++ src/Controller.java
@@ -0,0 +1,73 @@
+public class Controller {
+    public static void main(String[] args) {
+        boolean exit = false;
+
+        do {
+            System.out.println(
+                "Hola, ¿Qué operación quieres realizar?\n"
+                + "\t +, -, *, / \n"   
+            );
+            System.out.println(
+                "Pulsa q para salir\n"   
+            );
+            
+            CalcOperator operator = null;   // FIXME 
+            
+            do {
+                String readValue = System.console().readLine();
+                if (readValue.toLowerCase().compareTo("q") == 0) {
+                    exit = true;
+                }
+                else if (readValue.compareTo("+") == 0){
+                    operator = new Add();
+                }
+                else if (readValue.compareTo("-") == 0){
+                    operator = new Substract();
+                }
+                else if (readValue.compareTo("*") == 0){
+                    operator = new Multiplication();
+                }
+                else if (readValue.compareTo("/") == 0){
+                    operator = new Division();
+                }
+                else{
+                    System.out.println("Esa operación no está definida");
+                }
+            } while((operator == null)&&(!exit));
+
+            if(!exit){
+
+                System.out.println("\nEscriba el valor de a\n");
+                double a = readDouble();
+    
+                System.out.println("\nEscriba el valor de b\n");
+                double b = readDouble();
+                
+                double result = operator.operate(a, b);
+    
+                System.out.println("\nEl resultado es\n " + result);        
+            }
+        
+        } while (!exit);
+    }
+
+    
+    private static double readDouble() {
+        boolean is_double = false;
+        double     number    = 0;
+
+        do {
+            try {
+                number = Double.parseDouble(System.console().readLine());
+                is_double = true;
+            }
+            catch(NumberFormatException e){
+                System.out.println("Debes introducir un número");
+                is_double = false;
+            }
+        }while(!is_double);
+
+        return number;
+    }
+}
+
