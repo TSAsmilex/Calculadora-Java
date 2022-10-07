578bf5d9037d4f12b88783e9ab14059188f6bd1a
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-07 09:49:45 +0200

    Salir rápido del programa

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,56 +5,65 @@ public class Calculadora {
          * Leer los numeros a y b
          * Operar e imprimir la respuesta
         */
-        System.out.println(
-            "Hola, ¿Qué operación quieres realizar?\n"
-            + "\t +, -, *, / \n"   
-        );
-        
-        CalcOperator operator = null;   // FIXME
-        
+        boolean exit = false;
+
         do {
-            String readValue = System.console().readLine();
+            System.out.println(
+                "Hola, ¿Qué operación quieres realizar?\n"
+                + "\t +, -, *, / \n"   
+            );
+            System.out.println(
+                "Pulsa q para salir\n"   
+            );
             
-            if (readValue.compareTo("+") == 0){
-                operator = new Add();
-            }
-            else if (readValue.compareTo("-") == 0){
-                operator = new Substract();
-            }
-            else if (readValue.compareTo("*") == 0){
-                operator = new Multiplication();
-            }
-            else if (readValue.compareTo("/") == 0){
-                operator = new Division();
-            }
-            else{
-                System.out.println("Esa operación no está definida");
-            }
-        } while(operator == null);
+            CalcOperator operator = null;   // FIXME
+            
+            do {
+                String readValue = System.console().readLine();
+                if (readValue.toLowerCase().compareTo("q") == 0) {
+                    exit = true;
+                }
+                else if (readValue.compareTo("+") == 0){
+                    operator = new Add();
+                }
+                else if (readValue.compareTo("-") == 0){
+                    operator = new Substract();
+                }
+                else if (readValue.compareTo("*") == 0){
+                    operator = new Multiplication();
+                }
+                else if (readValue.compareTo("/") == 0){
+                    operator = new Division();
+                }
+                else{
+                    System.out.println("Esa operación no está definida");
+                }
+            } while((operator == null)&&(!exit));
 
+            if(!exit){
 
-        System.out.println("\nEscriba el valor de a\n");
+                System.out.println("\nEscriba el valor de a\n");
+                double a = readDouble();
+    
+                System.out.println("\nEscriba el valor de b\n");
+                double b = readDouble();
+                
+                double result = operator.operate(a, b);
+    
+                System.out.println("\nEl resultado es\n " + result);        
+            }
+        
+        } while (!exit);
+    }
 
+    
+    private static double readDouble() {
         boolean is_double = false;
-        double a = 0, b = 0;
+        double     number    = 0;
 
-        do  {
-            try {
-                a = Double.parseDouble(System.console().readLine());
-                is_double = true;
-            }
-            catch (NumberFormatException e) {
-                System.out.println("Debes introducir un número");
-                is_double = false;
-            }
-
-        } while (!is_double);
-
-        is_double=false;
-        System.out.println("\nEscriba el valor de b\n");
         do {
             try {
-                b = Double.parseDouble(System.console().readLine());
+                number = Double.parseDouble(System.console().readLine());
                 is_double = true;
             }
             catch(NumberFormatException e){
@@ -63,9 +72,6 @@ public class Calculadora {
             }
         }while(!is_double);
 
-        double result = operator.operate(a, b);
-
-        System.out.println("\nEl resultado es\n " + result);        
+        return number;
     }
-    
 }
