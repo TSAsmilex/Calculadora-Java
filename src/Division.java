4645528075dadf6e8fb3ac90c9b1e1e7525d0baa
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-07 09:27:03 +0200

    Excepción división

--- src/Division.java
+++ src/Division.java
@@ -1,6 +1,10 @@
 public class Division implements CalcOperator{
     @Override
     public double operate(double a, double b) {
+        if (Math.abs(b) <= 0.001) {
+            throw (new ArithmeticException("No se puede dividir por zero >:("));
+        }
+        
         return a/b;
     }
     
